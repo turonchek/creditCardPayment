2692c87bcb38ac14834922bb18dff3d4de4b9ba8
Elena <turonchek09@gmail.com>
2022-11-27 00:48:16 +0200

    form inputs were added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { FormBtn } from '../FormBtn/FormBtn';
 import { CardImg } from '../CardImg/CardImg'
+import { FormInputs } from '../FormInputs/FormInputs';
+import './Form.css'
 
 export const Form = () => {
     return (
         <form className='payment-page_form'>
             <CardImg/>
+            <FormInputs/>
             <FormBtn/>
         </form>
     );
---
+++ src/components/FormInputs/FormInputs.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './FormInputs.css'
+
+export const FormInputs = () => {
+    return (
+        <>
+            <div className='input__container mt'>
+                <h4 className='input__name'>Enter card number</h4>
+            </div>
+            <div className="input__container">
+                <h4 className='input__name'>Card Holder</h4>
+                <input className='form-input' type="text" placeholder="Please enter your full name" required/>
+            </div>
+            <div className="input-grp">
+                <div className="input__container">
+                    <h4 className='input__name'>Expiration Year</h4>
+                    <select className='form-select'>
+                        <option value="January">January</option>
+                        <option value="February">February</option>
+                        <option value="March">March</option>
+                        <option value="April">April</option>
+                        <option value="May">May</option>
+                        <option value="June">June</option>
+                        <option value="July">July</option>
+                        <option value="August">August</option>
+                        <option value="September">September</option>
+                        <option value="October">October</option>
+                        <option value="November">November</option>
+                        <option value="December">December</option>
+                    </select>
+                </div>
+                <div className="input__container">
+                    <h4 className='input__name'>Month</h4>
+                    <select className='form-select'>
+                        <option value="2021">2021</option>
+                        <option value="2022">2022</option>
+                        <option value="2023">2023</option>
+                        <option value="2024">2024</option>
+                        <option value="2025">2025</option>
+                        <option value="2026">2026</option>
+                        <option value="2027">2027</option>
+                        <option value="2028">2028</option>
+                        <option value="2029">2029</option>
+                        </select>
+                </div>
+                <div className="input__container">
+                    <h4 className='input__name'>CVV</h4>
+                    <input className='form-input' type="password" placeholder="CVV" required/>
+                </div>
+            </div>
+        </>
+    );
+}
+
